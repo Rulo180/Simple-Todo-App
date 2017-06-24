569581e6ff11cbb124e33a2adb2058472cc941c8
Rulo180 <rullencio180@gmail.com>
2017-06-24 14:28:53 -0300

    Step 1 to 4 Meteor ToDo App Tutorial

---
+++ imports/ui/body.js
@@ -0,0 +1,31 @@
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';    //Importo la coleccion de Tasks
+
+import './body.html';
+
+Template.body.helpers({       //Definimos un helper en el template body
+  tasks() {
+    return Tasks.find({}, { sort: { createdAt: -1 } });    //devuelvo el resultado del metodo find de Tasks
+  },
+});                           //Este helper devuelve un array de tasks
+
+Template.body.events({
+  'submit .new-task'(event) {         //Listener de evento submit de cualquier elemento que coincida con el selector CSS .new-task
+    //Prevent defaul browser form submit
+    event.preventDefault();
+
+    //Get value from form elemnent
+    const target = event.target;
+    const text = target.text.value;   //Almacena el valor del input en text
+
+    //Insert a taks into the Collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(),  //current time
+    });
+
+    //Clear form
+    target.text.value = '';
+  },
+});
---
+++ server/main.js
@@ -0,0 +1,7 @@
+import { Meteor } from 'meteor/meteor';
+
+import '../imports/api/tasks.js'    //Importo el modulo que contiene la coleccion de Tasks
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
