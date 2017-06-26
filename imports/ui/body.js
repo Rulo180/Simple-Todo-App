7d2a758f6b1b248252d3b7f5e7a6d735ed97da10
Rulo180 <rullencio180@gmail.com>
2017-06-26 00:17:27 -0300

    Step 7 to 8
    Temporary UI state
    Adding user accounts

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,14 +1,31 @@
+//Imports de modulos
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { Meteor } from 'meteor/meteor';
 
 import { Tasks } from '../api/tasks.js';    //Importo la coleccion de Tasks
 
+//Imports de templates
 import './body.html';
 import './task.js';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({       //Definimos un helper en el template body
   tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      //if hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } })
+    }
+    //Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });    //devuelvo el resultado del metodo find de Tasks
   },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  }
 });                           //Este helper devuelve un array de tasks
 
 Template.body.events({
@@ -23,10 +40,15 @@ Template.body.events({
     //Insert a taks into the Collection
     Tasks.insert({
       text,
-      createdAt: new Date(),  //current time
+      createdAt: new Date(),                //current time
+      owner: Meteor.userId(),               //devuelve el _id del usuario actual
+      username: Meteor.user().username,     //devuelve el documento user completo
     });
 
     //Clear form
     target.text.value = '';
   },
+  'click .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
+  }
 });
